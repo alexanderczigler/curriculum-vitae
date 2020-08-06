01c5535af67bc99897e261ee64aec2ffef661c03
Alexander Czigler <alexander@iteam.se>
2020-08-06 11:55:40 +0200

    fix: footer content

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -8,24 +8,35 @@
 
 <div class="section body web">
   <hr />
-  <p>Alexander Matthias Czigler 2020</p>
   <p>
+    Alexander Matthias Czigler 2020 |
     <a href="https://500px.com/alexanderczigler" target="_blank">
       <i class="fa fa-500px" />
     </a>
     <a href="https://github.com/alexanderczigler" target="_blank">
       <i class="fa fa-github" />
     </a>
-    <a href="https://hub.docker.com/u/aczigler" target="_blank">
-      <i class="fab fa-docker" />
-    </a>
     <a
       href="https://www.goodreads.com/user/show/53571813-alexander-czigler"
       target="_blank">
       <i class="fa fa-book" />
     </a>
+    <a href="https://hub.docker.com/u/aczigler" target="_blank">
+      <i class="fab fa-docker" />
+    </a>
     <a href="https://medium.com/@alexanderczigler" target="_blank">
       <i class="fa fa-medium" />
+    </a>
+    <a href="https://www.strava.com/athletes/60476104" target="_blank">
+      <i class="fa fa-stopwatch" />
+    </a>
+  </p>
+  <p>
+    <a
+      href="https://github.com/alexanderczigler/curriculum-vitae"
+      target="_blank">
+      <i class="fa fa-github" />
+      Source Code
     </a>
   </p>
 </div>
